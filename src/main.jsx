0bd5b47f4b22611e8eb71c3a144f589abc2b03b5
T Bhanu prasad <122262703+TeluguBhanuprasad123@users.noreply.github.com>
2023-06-25 20:57:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import './index.css'
 import Master from './compoents/Master.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
