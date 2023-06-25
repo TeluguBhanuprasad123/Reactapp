12d6929aa6958b4dce44629f7edf9283a80965e2
TeluguBhanuprasad123 <bhanuprasad50556@gmail.com>
2023-06-25 20:46:16 +0530

    bhanu first commit

--- src/compoents/Master.jsx
+++ src/compoents/Master.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  const Master=()=>{
     return(
         <>
- <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
+ <nav className="navbar navbar-expand-xl navbar-dark bg-dark" >
   <div className="container-fluid">
     <a className="navbar-brand" href="#">TeluguBhanuprasad</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
